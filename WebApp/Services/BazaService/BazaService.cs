a4a9974411cc1a68180e85d24ff5e6576c0b818c
AlexTrandafir09 <alexandru-ionut.trandafir@s.unibuc.ro>
2024-02-04 17:33:48 +0200

    create baza_sportiva service

---
+++ WebApp/Services/BazaService/BazaService.cs
@@ -0,0 +1,43 @@
+﻿using WebApp.Models.Baza_sportiva;
+using WebApp.Repositories.BazaRepository;
+
+namespace WebApp.Services.BazaService
+{
+    public class BazaService: IBazaService
+    {
+        private readonly IBazaRepository _bazaRepository;
+
+        public BazaService(IBazaRepository bazaRepository)
+        {
+            _bazaRepository = bazaRepository;
+        }
+
+        public async Task CreateBaza(Baza_sportiva baza)
+        {
+            await _bazaRepository.CreateAsync(baza);
+            await _bazaRepository.SaveAsync();
+        }
+
+        public async Task DeleteBaza(Baza_sportiva baza)
+        {
+            _bazaRepository.Delete(baza);
+            await _bazaRepository.SaveAsync();
+        }
+
+        public async Task<IEnumerable<Baza_sportiva>> GetAllBaze()
+        {
+            return await _bazaRepository.GetAllAsync();
+        }
+
+        public async Task<Baza_sportiva> GetBaza(Guid id)
+        {
+            return await _bazaRepository.FindByIdAsync(id);
+        }
+
+        public async Task UpdateBaza(Baza_sportiva baza)
+        {
+            _bazaRepository.Update(baza);
+            await _bazaRepository.SaveAsync();
+        }
+    }
+}
---
+++ WebApp/Services/BazaService/IBazaService.cs
@@ -0,0 +1,15 @@
+﻿using WebApp.Models.Baza_sportiva;
+
+namespace WebApp.Services.BazaService
+{
+    public interface IBazaService
+    {
+        Task<IEnumerable<Baza_sportiva>> GetAllBaze();
+        Task CreateBaza(Baza_sportiva baza);
+        Task<Baza_sportiva> GetBaza(Guid id);
+
+        Task DeleteBaza(Baza_sportiva baza);
+
+        Task UpdateBaza(Baza_sportiva baza);
+    }
+}
