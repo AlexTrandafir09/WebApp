d1bf9e5f83a90b52a92bdc0857be525813b3c4c7
AlexTrandafir09 <alexandru-ionut.trandafir@s.unibuc.ro>
2024-02-04 17:17:23 +0200

    create baza_sportiva repository

---
+++ WebApp/Repositories/BazaRepository/BazaRepository.cs
@@ -0,0 +1,13 @@
+﻿using WebApp.Data;
+using WebApp.Models.Baza_sportiva;
+using WebApp.Repositories.GenericRepository;
+
+namespace WebApp.Repositories.BazaRepository
+{
+    public class BazaRepository : GenericRepository<Baza_sportiva>, IBazaRepository
+    {
+        public BazaRepository(ApplicationDbContext applicationDbContext) : base(applicationDbContext)
+        {
+        }
+    }
+}
---
+++ WebApp/Repositories/BazaRepository/IBazaRepository.cs
@@ -0,0 +1,9 @@
+﻿using WebApp.Models.Baza_sportiva;
+using WebApp.Repositories.GenericRepository;
+
+namespace WebApp.Repositories.BazaRepository
+{
+    public interface IBazaRepository: IGenericRepository<Baza_sportiva>
+    {
+    }
+}
