5d1db12742fd667c024ae1a17606b31e6c002672
AlexTrandafir09 <alexandru-ionut.trandafir@s.unibuc.ro>
2024-02-04 16:32:16 +0200

    create echipa service, add function get, getall, create

---
+++ WebApp/Services/EchipaService/EchipaService.cs
@@ -0,0 +1,27 @@
+﻿using WebApp.Models.Echipa;
+using WebApp.Repositories.EchipaRepository;
+namespace WebApp.Services.EchipaService
+{
+    public class EchipaService : IEchipaService
+    {
+        private readonly IEchipaRepository _echipaRepository;
+
+        public EchipaService(IEchipaRepository echipaRepository)
+        {
+            _echipaRepository = echipaRepository;
+        }
+        public async Task<IEnumerable<Echipa>> GetAllEchipe()
+        {
+            return await _echipaRepository.GetAllAsync();
+        }
+        public async Task CreateEchipa(Echipa echipa)
+        {
+            await _echipaRepository.CreateAsync(echipa);
+            await _echipaRepository.SaveAsync();
+        }
+        public async Task<Echipa> GetEchipa(Guid id)
+        {
+            return await _echipaRepository.FindByIdAsync(id);
+        }
+    }
+}
---
+++ WebApp/Services/EchipaService/IEchipaService.cs
@@ -0,0 +1,14 @@
+﻿using WebApp.Models.Echipa;
+using WebApp.Repositories.EchipaRepository;
+
+namespace WebApp.Services.EchipaService
+{
+    public interface IEchipaService
+    {
+        Task<IEnumerable<Echipa>> GetAllEchipe();
+        Task CreateEchipa(Echipa echipa);
+        Task<Echipa> GetEchipa(Guid id);
+
+
+    }
+}
