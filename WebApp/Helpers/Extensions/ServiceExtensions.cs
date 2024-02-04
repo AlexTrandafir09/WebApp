5dec434e9c427a0948c23cff3bf91b022908b0cf
AlexTrandafir09 <alexandru-ionut.trandafir@s.unibuc.ro>
2024-02-04 21:27:03 +0200

    add service extensions

---
+++ WebApp/Helpers/Extensions/ServiceExtensions.cs
@@ -0,0 +1,42 @@
+﻿using WebApp.Repositories.BazaRepository;
+using WebApp.Repositories.Echipa_ligaRepository;
+using WebApp.Repositories.EchipaRepository;
+using WebApp.Repositories.JucatorRepository;
+using WebApp.Repositories.LigaRepository;
+using WebApp.Services.BazaService;
+using WebApp.Services.Echipa_ligaService;
+using WebApp.Services.EchipaService;
+using WebApp.Services.JucatorService;
+
+namespace WebApp.Helpers.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static IServiceCollection AddRepositories(this IServiceCollection services)
+        {
+            //services.AddTransient<IRepository, Repository>();
+            services.AddTransient<IBazaRepository, BazaRepository>();
+            services.AddTransient<IEchipaRepository, EchipaRepository>();
+            services.AddTransient<IEchipa_ligaRepository, Echipa_ligaRepository>();
+            services.AddTransient<IJucatorRepository, JucatorRepository>();
+            services.AddTransient<ILigaRepository, LigaRepository>();
+
+
+            return services;
+        }
+
+        public static IServiceCollection AddServices(this IServiceCollection services)
+        {
+            //services.AddTransient<IService,Service>();
+            services.AddTransient<IBazaService, BazaService>();
+            services.AddTransient<IEchipaService, EchipaService>();
+            services.AddTransient<IEchipa_ligaService,Echipa_ligaService>();
+            services.AddTransient<IJucatorService,JucatorService>();
+            services.AddTransient<ILigaRepository,LigaRepository>();
+
+
+
+            return services;
+        }
+    }
+}
