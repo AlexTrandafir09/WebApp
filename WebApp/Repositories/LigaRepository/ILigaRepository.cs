b0fe603ae44fb475f93c5a9da831db2767303902
AlexTrandafir09 <alexandru-ionut.trandafir@s.unibuc.ro>
2024-02-04 18:09:37 +0200

    create liga repository

---
+++ WebApp/Repositories/LigaRepository/ILigaRepository.cs
@@ -0,0 +1,9 @@
+﻿using WebApp.Models.Liga;
+using WebApp.Repositories.GenericRepository;
+
+namespace WebApp.Repositories.LigaRepository
+{
+    public interface ILigaRepository: IGenericRepository<Liga>
+    {
+    }
+}
---
+++ WebApp/Repositories/LigaRepository/LigaRepository.cs
@@ -0,0 +1,13 @@
+﻿using WebApp.Data;
+using WebApp.Models.Liga;
+using WebApp.Repositories.GenericRepository;
+
+namespace WebApp.Repositories.LigaRepository
+{
+    public class LigaRepository : GenericRepository<Liga>, ILigaRepository
+    {
+        public LigaRepository(ApplicationDbContext applicationDbContext) : base(applicationDbContext)
+        {
+        }
+    }
+}
