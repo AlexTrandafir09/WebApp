6dcf4b796337c0d4281ea5a389979b47308250b7
AlexTrandafir09 <alexandru-ionut.trandafir@s.unibuc.ro>
2024-02-04 17:40:29 +0200

    create jucator repository

---
+++ WebApp/Repositories/JucatorRepository/IJucatorRepository.cs
@@ -0,0 +1,9 @@
+﻿using WebApp.Models.Jucator;
+using WebApp.Repositories.GenericRepository;
+
+namespace WebApp.Repositories.JucatorRepository
+{
+    public interface IJucatorRepository :IGenericRepository<Jucator>
+    {
+    }
+}
---
+++ WebApp/Repositories/JucatorRepository/JucatorRepository.cs
@@ -0,0 +1,13 @@
+﻿using WebApp.Data;
+using WebApp.Models.Jucator;
+using WebApp.Repositories.GenericRepository;
+
+namespace WebApp.Repositories.JucatorRepository
+{
+    public class JucatorRepository : GenericRepository<Jucator>, IJucatorRepository
+    {
+        public JucatorRepository(ApplicationDbContext applicationDbContext) : base(applicationDbContext)
+        {
+        }
+    }
+}
