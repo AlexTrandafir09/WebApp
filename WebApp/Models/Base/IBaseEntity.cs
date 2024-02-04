114c9536ccf051d52bf58bc01a077d08a737240a
AlexTrandafir09 <alexandru-ionut.trandafir@s.unibuc.ro>
2024-02-04 15:39:50 +0200

    add base entity

---
+++ WebApp/Models/Base/IBaseEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApp.Models.Base
+{
+    public interface IBaseEntity
+    {
+        Guid Id { get; set; }
+        DateTime CreatedAt { get; set; }
+        DateTime UpdatedAt { get; set; }
+    }
+}
