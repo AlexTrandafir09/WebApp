d2f92c774a6add7f8ad04ba37541f7ec0d6b35fc
AlexTrandafir09 <alexandru-ionut.trandafir@s.unibuc.ro>
2024-02-04 15:38:36 +0200

    link host in main program.cs

--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -1,11 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using WebApp.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+       options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 var app = builder.Build();
 
