40d4d1936ee27683a90a90aae498148eaf1a6508
AlexTrandafir09 <alexandru-ionut.trandafir@s.unibuc.ro>
2024-02-04 16:07:27 +0200

    add echipa repository + inherit from generic repository

---
+++ WebApp/Repositories/EchipaRepository/EchipaRepository.cs
@@ -0,0 +1,13 @@
+﻿using WebApp.Data;
+using WebApp.Models.Echipa;
+using WebApp.Repositories.GenericRepository;
+
+namespace WebApp.Repositories.EchipaRepository
+{
+    public class EchipaRepository : GenericRepository<Echipa>, IEchipaRepository
+    {
+        public EchipaRepository(ApplicationDbContext applicationDbContext) : base(applicationDbContext)
+        {
+        }
+    }
+}
---
+++ WebApp/Repositories/EchipaRepository/IEchipaRepository.cs
@@ -0,0 +1,9 @@
+﻿using WebApp.Models.Echipa;
+using WebApp.Repositories.GenericRepository;
+
+namespace WebApp.Repositories.EchipaRepository
+{
+    public interface IEchipaRepository : IGenericRepository<Echipa>
+    {
+    }
+}
