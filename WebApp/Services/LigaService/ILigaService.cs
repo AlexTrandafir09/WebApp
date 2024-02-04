7e4f245fc462f198117816f464a2d0f298f40e9d
AlexTrandafir09 <alexandru-ionut.trandafir@s.unibuc.ro>
2024-02-04 18:16:03 +0200

    create liga service (CRUD)

---
+++ WebApp/Services/LigaService/ILigaService.cs
@@ -0,0 +1,15 @@
+﻿using WebApp.Models.Liga;
+
+namespace WebApp.Services.LigaService
+{
+    public interface ILigaService
+    {
+        Task<IEnumerable<Liga>> GetAllLigi();
+        Task CreateLiga(Liga liga);
+        Task<Liga> GetLiga(Guid id);
+
+        Task DeleteLiga(Liga liga);
+
+        Task UpdateLiga(Liga liga);
+    }
+}
---
+++ WebApp/Services/LigaService/LigaService.cs
@@ -0,0 +1,44 @@
+﻿using WebApp.Models.Jucator;
+using WebApp.Models.Liga;
+using WebApp.Repositories.LigaRepository;
+
+namespace WebApp.Services.LigaService
+{
+    public class LigaService:ILigaService
+    {
+        private readonly ILigaRepository _ligaRepository;
+
+        public LigaService(ILigaRepository ligaRepository)
+        {
+            _ligaRepository = ligaRepository;
+        }
+
+        public async Task CreateLiga(Liga liga)
+        {
+            await _ligaRepository.CreateAsync(liga);
+            await _ligaRepository.SaveAsync();
+        }
+
+        public async Task DeleteLiga(Liga liga)
+        {
+            _ligaRepository.Delete(liga);
+            await _ligaRepository.SaveAsync();
+        }
+
+        public async Task<IEnumerable<Liga>> GetAllLigi()
+        {
+            return await _ligaRepository.GetAllAsync();
+        }
+
+        public async Task<Liga> GetLiga(Guid id)
+        {
+            return await _ligaRepository.FindByIdAsync(id);
+        }
+
+        public async Task UpdateLiga(Liga liga)
+        {
+            _ligaRepository.Update(liga);
+            await _ligaRepository.SaveAsync();
+        }
+    }
+}
