e4a8e125354aedffccf1327990b316ed6e91e5ba
AlexTrandafir09 <alexandru-ionut.trandafir@s.unibuc.ro>
2024-02-04 17:09:19 +0200

    first migration

--- WebApp/Data/ApplicationDbContext.cs
+++ WebApp/Data/ApplicationDbContext.cs
@@ -1,7 +1,12 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using WebApp.Models.Echipa;
 namespace WebApp.Data
 {
     public class ApplicationDbContext: DbContext
-    {
+    {   
+        public DbSet<Echipa> Echipe { get; set; }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
     }
 }
